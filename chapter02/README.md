b0d22576f9f7be11e88d0ada0be5112d078585ef
jack-zheng <lanmolei0124@163.com>
2019-03-01 23:51:05 +0800

    finish chapter02, some conception is not farmiliar to me

--- chapter02/README.md
+++ chapter02/README.md
@@ -4,7 +4,15 @@
 1. not sure how to use the `after_this_request` decorator, sample failed
 1. chrome URL 地址栏左边的小叹号可以看cookie信息
 1. cookie 很容易作伪，像登陆凭证社么的需要加密使用 - session
-
+1. flask 的四种上下文变量: current_app, g, request, session
+1. 科普了一下重定向漏洞及其解决方案
+1. 科普了一下Web 安全
+    + SQL 注入, 输入 "' or 1=1 --" 或者 "'; drop table student; --" 已达到返回所有值或者删除表的功能
+    + 对策: ORM， 输入类型验证， 参数化查询(sql + ? 而不是拼接), 转义特殊字符
+    + XSS(反射型/存储行)
+    + 对策：HTML转义，输入验证
+    + CSRF 攻击
+    + 对策：使用正确的HTTP方法，CSRF令牌校验
 ## How To Run Chatper Sample
 
 cd to chapter02 folder, run command `flask run`, app will start.
--- chapter02/app.py
+++ chapter02/app.py
@@ -1,10 +1,40 @@
 from flask import Flask, request, redirect, jsonify
 from flask import make_response, url_for, session, abort
 from datetime import datetime
+from urllib.parse import urlparse, urljoin
+from jinja2.utils import generate_lorem_ipsum
+
 
 app = Flask(__name__)
 # just for testing, set it to .env file when production env
 app.secret_key = 'secret'
+
+
+@app.route('/post')
+def show_post():
+    post_body = generate_lorem_ipsum(n=2)
+    return '''
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <h1>Very long post</h1>
+    <div class="body">%s</div>
+    <button id="load">Load More</button>
+    <script type="text/javascript">
+    $( "#load" ).click(function() {
+        $.ajax({
+                    url: '/more',
+                    type: 'get',
+                    success: function(data){
+                        $('.body').append(data);
+                    }
+                })
+    });
+    </script>
+    ''' % post_body
+
+
+@app.route('/more')
+def load_post():
+    return generate_lorem_ipsum(n=1)
 
 
 @app.route('/logout')
@@ -86,3 +116,40 @@ def redirect02():
 @app.route('/foo')
 def foo():
     return jsonify(name='jack', age='20')
+
+
+@app.route('/bar')
+def bar():
+    return '<h1>Foo5101</h1><a href="%s">Do something and redirect</a>'\
+        % url_for('do_something', next=request.full_path)
+
+
+def redirect_back(default='hello', **kwargs):
+    print('next value: %s' % request.args.get('next'))
+    for target in request.args.get('next'), request.referrer:
+        if target:
+            print('target = %s' % target)
+            if is_safe_url(target):
+                return redirect(target)
+    return redirect(url_for(default, **kwargs))
+
+
+@app.route('/do_something_and_redirect')
+def do_something():
+    print("Into do something method")
+    return redirect_back()
+
+
+@app.route('/referrer')
+def test_refer():
+    return '<h1>Referrer Test</h1><a href="%s">Go to bar page</a>'\
+        % url_for('bar', next=request.full_path)
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    print('request host url: %s' % request.host_url)
+    print('netloc = %s | %s' % (ref_url.netloc, test_url.netloc))
+    return test_url.scheme in ('http', 'https') and \
+        ref_url.netloc == test_url.netloc

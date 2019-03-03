e6b46fd9d53aef788ba501b5a68b650c89ea906e
jack-zheng <lanmolei0124@163.com>
2019-03-03 17:01:05 +0800

    finish chapter4

--- chapter04/app.py
+++ chapter04/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, flash, redirect, url_for
-from flask import session, send_from_directory
-from forms import LoginForm, UploadForm
+from flask import session, send_from_directory, request
+from forms import LoginForm, UploadForm, MultiUploadForm, RichTextForm
+from forms import NewPostForm, SigninForm, RegisterForm
+from forms import SigninForm2, RegisterForm2
+from flask_wtf.csrf import validate_csrf
+from wtforms import ValidationError
+from flask_ckeditor import CKEditor
 import os
 import uuid
 
@@ -9,6 +14,137 @@ app = Flask(__name__)
 # just for testing, set it to .env file when production env
 app.secret_key = 'secret04'
 app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')
+app.config['ALLOWED_EXTENSIONS'] = ['png', 'jpg', 'jpeg', 'gif']
+app.config['CHEDITOR_SERVE_LOCAL'] = True
+
+ckeditor = CKEditor(app)
+
+
+@app.route('/dropzone-upload', methods=['GET', 'POST'])
+def dropzone_upload():
+    pass
+
+
+@app.route('/custom-validator', methods=['GET', 'POST'])
+def custom_validator():
+    pass
+
+
+@app.route('/multi-form', methods=['GET', 'POST'])
+def multi_form():
+    signin_form = SigninForm()
+    register_form = RegisterForm()
+
+    if signin_form.submit1.data and signin_form.validate():
+        username = signin_form.username.data
+        flash('%s, you just submit the signin form.' % username)
+        return redirect(url_for('index'))
+
+    if register_form.submit2.data and register_form.validate():
+        username = register_form.username.data
+        flash('%s, you just submit the register form.' % username)
+        return redirect(url_for('index'))
+
+    return render_template(
+        '2form.html', signin_form=signin_form, register_form=register_form)
+
+
+@app.route('/multi-form-multi-view')
+def multi_form_multi_view():
+    signin_form = SigninForm2()
+    register_form = RegisterForm2()
+    return render_template(
+        '2form2view.html', signin_form=signin_form, register_form=register_form
+    )
+
+
+@app.route('/handle-signin', methods=['POST'])
+def handle_signin():
+    signin_form = SigninForm2()
+    register_form = RegisterForm2()
+
+    if signin_form.validate_on_submit():
+        username = signin_form.username.data
+        flash('%s, you just submit the Signin Form.' % username)
+        return redirect(url_for('index'))
+
+    return render_template(
+        '2form2view.html',
+        signin_form=signin_form, register_form=register_form)
+
+
+@app.route('/handle-register', methods=['POST'])
+def handle_register():
+    signin_form = SigninForm2()
+    register_form = RegisterForm2()
+
+    if register_form.validate_on_submit():
+        username = register_form.username.data
+        flash('%s, you just submit the Register Form.' % username)
+        return redirect(url_for('index'))
+    return render_template(
+        '2form2view.html',
+        signin_form=signin_form, register_form=register_form)
+
+
+@app.route('/two-submits', methods=['GET', 'POST'])
+def two_submits():
+    form = NewPostForm()
+    if form.validate_on_submit():
+        if form.save.data:
+            print('Save post')
+            flash('You click the save button')
+        elif form.publish.data:
+            print('Publish post')
+            flash('You click the publish button')
+        return redirect(url_for('index'))
+    return render_template('2submit.html', form=form)
+
+
+@app.route('/ckeditor', methods=['GET', 'POST'])
+def integrate_ckeditor():
+    form = RichTextForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        flash('Your post is published.')
+        return render_template('post.html', title=title, body=body)
+    return render_template('ckeditor.html', form=form)
+
+
+@app.route('/multi-upload', methods=['GET', 'POST'])
+def multi_upload():
+    form = MultiUploadForm()
+
+    if request.method == 'POST':
+        filenames = []
+        try:
+            validate_csrf(form.csrf_token.data)
+        except ValidationError:
+            flash('CSRF token error')
+            return redirect(url_for('multi_upload'))
+
+        if 'photo' not in request.files:
+            flash('This field is required.')
+            return redirect(url_for('multi_upload'))
+
+        for f in request.files.getlist('photo'):
+            if f and allowed_file(f.filename):
+                filename = random_filename(f.filename)
+                f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+                filenames.append(filename)
+            else:
+                flash('Invalid file type')
+                return redirect(url_for('multi_upload'))
+        flash('Upload success.')
+        session['filenames'] = filenames
+        return redirect(url_for('show_images'))
+    return render_template('upload.html', form=form)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
 @app.route('/uploads/<path:filename>')
--- chapter04/forms.py
+++ chapter04/forms.py
@@ -1,8 +1,64 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, ValidationError
-from wtforms import IntegerField
+from wtforms import TextAreaField
+from wtforms.validators import DataRequired, Length, ValidationError, Email
+from wtforms import IntegerField, MultipleFileField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask_ckeditor import CKEditorField
+
+
+class SigninForm2(FlaskForm):
+    username = StringField(
+        'Username', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(8, 128)])
+    submit = SubmitField('Sign in')
+
+
+class RegisterForm2(FlaskForm):
+    username = StringField(
+        'Username', validators=[DataRequired(), Length(1, 20)])
+    email = StringField(
+        'Email', validators=[DataRequired(), Email(), Length(1, 20)])
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(8, 128)])
+    submit = SubmitField('Register')
+
+
+class SigninForm(FlaskForm):
+    username = StringField(
+        'Username', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(8, 128)])
+    submit1 = SubmitField('Sign in')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        'Username', validators=[DataRequired(), Length(1, 20)])
+    email = StringField(
+        'Email', validators=[DataRequired(), Email(), Length(1, 20)])
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(8, 128)])
+    submit2 = SubmitField('Register')
+
+
+class NewPostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(1, 50)])
+    body = TextAreaField('Body', validators=[DataRequired()])
+    save = SubmitField('Save')
+    publish = SubmitField('Publish')
+
+
+class RichTextForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(1, 50)])
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField('Publish')
+
+
+class MultiUploadForm(FlaskForm):
+    photo = MultipleFileField('Upload Image', validators=[DataRequired()])
+    submit = SubmitField()
 
 
 class UploadForm(FlaskForm):
